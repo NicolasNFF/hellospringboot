257477f2042fa3304e7819bbcbe2b71882dc96c4
Nicolas Ferrari <nicolassnfz.12@gmail.com>
2023-12-03 18:23:33 -0300

    delete

--- src/main/java/br/com/fatec/hellospringboot/entities/Client.java
+++ src/main/java/br/com/fatec/hellospringboot/entities/Client.java
@@ -1,6 +1,8 @@
 package br.com.fatec.hellospringboot.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -8,32 +10,35 @@ import jakarta.persistence.Table;
 @Table(name = "TBL_CLIENT")
 public class Client {
 
-
-    @Id// definindo chave primaria
+    @Id // definindo chave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gerar chave primaria de forma automatica
     private int id;
 
     private String name;
     private double balance;
 
-
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public double getBalance() {
         return balance;
     }
+
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
-    
 }
